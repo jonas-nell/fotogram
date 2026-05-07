79747eb0d490ae70ff08fa969e1ba2c7a9e9e4d7
Jonas Nell <jonasnell1705@gmail.com>
2026-05-07 22:07:32 +0200

    added unstyled dialog and gifs

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const images = [
     "001.jpg",
     "002.jpg",
+    "kek-wiggle.gif",
     "003.jpg",
     "004.jpg",
+    "pog-o.gif",
     "005.jpg",
     "006.jpg",
     "007.jpg",
+    "kid_pc_thumbs_up_brent_rambo.gif",
     "008.jpg",
     "009.jpg",
     "010.jpg",
-    "011.webp",
+    "011.webp"
 ];
+
+const dialogRef = document.getElementById("img-pop-up");
+const dialogImg = document.getElementById("dialog-img");
+const dialogTitle = document.getElementById("img-title");
 
 
 function render() {
@@ -19,7 +26,19 @@ function render() {
 
     for (const [index, image] of images.entries()){
         gallery.innerHTML += /*html*/`
-            <img src="./assets/img/${image}" alt="">
+            <img onclick="openDialog(${index})" src="./assets/img/${image}" alt="">
         `;
     }
+}
+
+function openDialog(index, image){
+    const clickedImg = images[index];
+    dialogImg.src =`./assets/img/${clickedImg}`;
+    dialogTitle.innerHTML = clickedImg;
+    dialogRef.showModal();
+
+}
+
+function closeDialog(){
+    dialogRef.close();
 }
